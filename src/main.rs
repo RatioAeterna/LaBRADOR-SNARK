b3a73e92ed032de2cbb60d69bbad1be3fff11c81
RatioAeterna <jasongenecox@protonmail.com>
2023-04-05 11:51:00 -0500

    Changes from previous commit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,40 @@ use rand::distributions::Uniform;
 use na::base::DVector;
 use ndarray::{Array2, concatenate};
 
+pub fn generate_polynomial() -> Polynomial<i32> {
+    let mut poly = Polynomial::new();
+    let num_terms = rng.gen_range(1, max_degree + 2);
+    for degree in 0..num_terms {
+        let coeff = rng.gen_range(0, q);
+        p.add_coeff(degree, coeff);
+    }
+    poly
+}
+
+
+// Computes the ring inverse of a polynomial element of R_q
+pub fn conjugation_automorphism(a : Polynomial) -> Polynomial {
+
+
+
+}
+
+
+
+
+
+// compute the dot product between two lists of 
+pub fn polynomial_vec_inner_product(v1, v2) -> Polynomial<i32> {
+    let result = Polynomial::new();
+    for i in v1.length() {
+        let product = v1[i].mul(&v2[i]);
+        result.add(product);
+    }
+    result
+}
+
+
+
 pub fn create_proof() -> Result<Proof> {
 
     // compute witness: r vectors s_1, ..., s_r in R_q^n such that
@@ -20,9 +54,84 @@ pub fn create_proof() -> Result<Proof> {
     // R_q = Z_q[x]/(x^d+1) "polynomials with prime order integer coefficients mod x^d+1"
     // e.g., p = 7, x^3+1
 
-    let b = Polynomial::new(vec)
 
-    let s = Array2::<i32>::zeros((n,r)); // don't know what the witness is yet
+    // TODO: these values are totally random at the moment
+
+    // q: modulus of the ring of integers
+    let q : i32 = 128;
+    
+    // constants: totally random for now and should be changed later
+    let n : i32 = 128;
+    let r : i32 = 20;
+
+
+    // setting bound for SIS 2-norm
+    let beta_bound : i32 = 65536
+
+    let S= Array2::from_elem((n,r), Polynomial::new()); 
+
+    // Random Generation of Witness matrix S
+    
+    // total summed norm:
+    let mut norm_sum = 0;
+
+
+    for i in 1..r {
+        for j in 1..n {
+            let s_ij = generate_polynomial();
+            S[[i,j]] = s_ij;
+            // add norm to norm sum
+            norm_sum += i32::pow(s_ij.norm(), 2);
+        }
+    }
+
+    // if too big, scale down:
+    if norm_sum > i32::pow(beta_bound,2) {
+        let scale_factor = i32::pow(beta_bound,2) / norm_sum;
+        // TODO for all polynomials: *p *= scale;
+    }
+
+
+
+    // random generation of the symmetric polynomial matrix A
+    let A = Array2::from_elem((r,r), Polynomial::new()); 
+    for i in 0..r {
+        for j in 0..r {
+            let a_ij = generate_polynomial();
+            if A[[i,j]] == Polynomial::new() {
+                A[[i,j]] = a_ij;
+                A[[j,i]] = a_ij;
+            }
+        }
+    }
+
+    // random generation of random polynomial matrix Phi
+    let Phi = Array2::from_elem((n,r), Polynomial::new()); 
+    for i in 0..n {
+        for j in 0..r {
+            let phi_ij = generate_polynomial();
+            Phi[[i,j]] = phi_ij;
+        }
+    }
+
+
+    // compute a_product and phi_product:
+    let a_product : Polynomial = Polynomial::new();
+    for i in 1..r {
+        for j in 1..r {
+            let inner_prod = polynomial_vec_inner_product(S.column_mut(i), S.column_mut(j));
+            let prod = A[[i,j]].mul(&inner_prod);
+            a_product.add(&prod);
+        }
+    }
+
+    let phi_product : Polynomial = Polynomial::new();
+    for i in 1..r {
+        let inner_prod = polynomial_vec_inner_product(Phi.column_mut(i), S.column_mut(i));
+        phi_product.add(&inner_prod);
+    }
+
+    let b : Polynomial = a_product.add(&phi_product);
 
     let t = Array2::<i32>::zeros((m,r)); // Inner commitment vector 
 
@@ -30,16 +139,15 @@ pub fn create_proof() -> Result<Proof> {
     // Compute inner Ajtai commitments
     // t_i = As_i \in R_q^\kappa (should just be m-dimensional)
     for i in 0..r {
-        let t_i = A.matmul(&s.column(i)); 
+        // TODO this only works if we switch to using MatrixMN instead of Array2.
+        let t_i = A * s.column(i);
         t.column_mut(i).assign(&t_i);
     }
 
     // Compute outer commitment u_1
-    let u_1 = b.dot(&t);
-
+    let u_1 = b * t;
 
     // JL projection
-
 
 
     let projected_vec = jl_projection(&s[..]);
@@ -79,10 +187,10 @@ pub fn binary_r1cs_reduction(A, B, C, w) {
     }
 
 
-
-
-
-
+    let a_inv = conjugation_automorphism(a);
+    let b_inv = conjugation_automorphism(b);
+    let c_inv = conjugation_automorphism(c);
+    let w_inv = conjugation_automorphism(w);
 }
 
 
@@ -96,8 +204,11 @@ pub fn arithmetic_r1cs_reduction() {
 
 
 
-
+// generates and returns the SIS vector s
 pub fn generate_witness() {
+    
+
+
 
 
 
